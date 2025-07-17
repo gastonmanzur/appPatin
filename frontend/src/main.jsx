e7f0e6480861358e287f60cee1dff9dfced1b3ec
gastonmanzur <gastonmanzur@.com>
2025-07-17 19:38:01 -0300

    Commit deploy vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import './index.css';
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 if (!clientId) {
@@ -14,20 +13,11 @@ if (!clientId) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
- <GoogleOAuthProvider clientId="483587451822-fjrc9gpgaegbbh1pvlbq8qpbc9sauve1.apps.googleusercontent.com">
- <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId || ''}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </GoogleOAuthProvider>
-
   </React.StrictMode>
-  </GoogleOAuthProvider>
 );
