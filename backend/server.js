9d3b122249abe22042a314e8704f37816b2eeaf7
gastonmanzur <57376378+gastonmanzur@users.noreply.github.com>
2025-07-20 20:11:24 -0300

    Fix incorrect CORS header middleware

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,11 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res,next)=>{
-  Header set Access-Control-Allow-Origin "*"
-    Header set Access-Control-Allow-Methods "GET, POST, OPTIONS"
-    Header set Access-Control-Allow-Headers "Content-Type, Authorization" 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 // ---------- Configuración CORS ----------
