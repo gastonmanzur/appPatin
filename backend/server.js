ce354d6069997e65ebd6a4a1098e63efc018e6d9
gastonmanzur <57376378+gastonmanzur@users.noreply.github.com>
2025-07-20 20:03:47 -0300

    Update server.js2

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res,next)=>{
+  Header set Access-Control-Allow-Origin "*"
+    Header set Access-Control-Allow-Methods "GET, POST, OPTIONS"
+    Header set Access-Control-Allow-Headers "Content-Type, Authorization" 
+});
 
 // ---------- Configuración CORS ----------
 const allowedOrigins = process.env.ALLOWED_ORIGINS
